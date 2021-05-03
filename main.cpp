c3a76cebf71ac78c488972711edbbeacdf15c22a
ArchKats <>
2021-05-03 18:28:55 +0000

    Small fixes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
+/* Temporary Placeholder main that is used for testing*/
+#include"Classes/Array.h"
 #include<iostream>
+#include<fstream>
 
 int main(){
-    int test;
+    std::ifstream file;
+    file.open("Classes/test.txt");
+
+
+    Array l(file);
+
+    l.print();
     
 }
