156f871944896ccadd8fdb4e86334601a294e4c0
ArchKats <Chriskats02m@gmail.com>
2021-05-15 02:13:31 +0300

    Finished (?) Array Class

--- Classes/Array.cpp
+++ Classes/Array.cpp
@@ -7,27 +7,27 @@
 
 Array::Array(std::ifstream& f){
 
-    std::ifstream file = ClearFile(f);
+    std::ifstream file = ClearFile(f); // Creation of a punctuation-free copy of the initial file, This probably at some point will be moved to the main function
 
     std::string entry; // String Variable that will contain each temporary word of the file
     NUM = 0; // Reseting the size of the array
 
+    // First Entry to the array is out of the loop in order to make the code easier to read and understand
     file >> entry;
     entry = better2low(entry.c_str());
     append(entry);
+
     while(file >> entry){ // Loop that continues till every word is imported into the array
         entry = better2low(entry.c_str()); // Converting the entry string into lower case for more information check the local function library
-        //std::cout << NUM << " " << entry << std::endl; // Temporary output
-        std::cout << NUM <<" "<< entry<< std::endl;
-        int pos = search(entry);
+        int pos = search(entry); // Searches for the entry in the array, if it is found the function returns the position of the word into the integer variable pos, if it was not found it returns the negative value -1 into the pos variable 
         if(pos!=-1){ // case for the word being found in the array
             FREQ[pos]++; // increasing the frequency of the word
         }
-        else{ //if(NUM<2) GARBAGE
+        else{ // case for the word not being found in the array and thus appening it into the array
             append(entry); // inserting the entry string into the array
         }
     }
-    std::cout << "Array's Construction was Successful" << std::endl;
+    std::cout << "Array's Construction was Successful" << std::endl; // Temporary output to sign the Finishing of the Array's construction
 }
 
 Array::~Array(){ 
@@ -38,36 +38,60 @@ Array::~Array(){
 void Array::append(const std::string entry){
     NUM++; // increasing the integer that represents the size of the array
     // Temporary arrays that are copies of the original arrays (WORDS and FREQ)
-    std::string* string_temp;
-    int* int_temp;
-    string_temp = new std::string[NUM-1];
-    int_temp = new int[NUM-1];
+    std::string* string_temp = new std::string[NUM];
+    int* int_temp = new int[NUM];
     for(int i=0; i<NUM-1; i++){
         string_temp[i] = WORDS[i];
         int_temp[i] = FREQ[i];
     }
+    string_temp[NUM-1]=entry;
+    int_temp[NUM-1]=0;
     
     // Deletion of the previous arrays
     delete[] WORDS;
     delete[] FREQ;
-    // Memory Allocation for the new "appended" arrray
-    WORDS = new std::string[NUM];
-    FREQ = new int[NUM];
     // Copying the temporary arrays into the new ones
-    for(int i=0; i<NUM-1; i++){
-        WORDS[i] = string_temp[i];
-        FREQ[i] = int_temp[i];
+    WORDS = string_temp;
+    FREQ = int_temp;
+}
+
+void Array::remove(const std::string del){
+
+    //Temporary arrays that are diminished-by-one from the size of the original arrays
+
+    int pos = search(better2low(del.c_str())); // Search for the position of the word
+    
+    if(pos!=-1){ // Case of the position being found
+        NUM--; // diminishing the integer that represents the size of the array
+        std::string* string_temp = new std::string[NUM];
+        int* int_temp =new int[NUM];
+        int i = 0;
+        int j = 0;
+        while(j<NUM){ // loop that inserts in the temporary arrays all Words and Frequencies but those of the ones that are to be removed
+            if(i==pos) i++;
+            else{
+                string_temp[j]=WORDS[i];
+                int_temp[j]=FREQ[i];
+                j++;
+                i++;
+            }
+        }
+        // Deletion of the previous arrays
+        delete[] WORDS;
+        delete[] FREQ;
+        
+        // Copying the temporary arrays into the new ones
+        WORDS = string_temp;
+        FREQ = int_temp;
     }
-    //inserting the entry word into the new array
-    WORDS[NUM-1]=entry;
-    FREQ[NUM-1]=1;
+    else std::cout << "!!! ERROR WORD WAS NOT FOUND IN ARRAY !!!"; // Error message for the case in which the position of the word was not found
 }
 
 int Array::search(const std::string key){ // Just a linear search of the key string
     for(int i=0;i<NUM;i++){
         if(WORDS[i]==key) return i; // Case in which the key string was found
     }
-    return -1; // b
+    return -1; // Case in which the key string was not found
 }
 
 
