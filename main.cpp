5406de676d13da4223a0b7f728eb24bf595c953c
ArchKats <>
2021-05-21 20:32:41 +0000

    Main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,48 @@
-/* Temporary Placeholder main that is used for testing*/
-#include"Classes/Array.h"
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+using namespace std;
 
+void createQ(string* Q, int size, char* file_name);
+int main()
+{
+	srand((unsigned)time(0));
+	char file_name[40];
+	cin >> file_name;
+	ifstream ifs;
+	ifs.open(file_name);
+	if (ifs.is_open())
+	{
+		cout << "File opened\n";
+	}
+	else cout << "File did not open";
+	string a;
+	 const int size =rand() ;
+	string * Q;
+	Q = new string[size];	
+	createQ(Q, size,file_name);
 
-int main(){
-    std::ifstream file;
-    file.open("test.txt");
+	for (int i = 0; i <size; i++)
+	{
+		cout << Q[i]<<" ";
+	}
+}
+void createQ(string* Q, int size,char * file_name)
+{
+		int i = 0;
+		ifstream ifs;
+		string a;
+		ifs.open(file_name);
+		
+	while (ifs >> a&& i<size)
+	{
+		if ((rand()) % 2 == 1)
+		{
+			Q[i] = a;
+			i++;
+		}
 
-    std::cout << "FILE OPEN";
+	}
 
-    Array l(file);
-
-    l.print(); 
-    
-    std::cout << "FULE";
-    remove("Temp.txt");
 }
